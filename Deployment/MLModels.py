8b84fb87ef1929e81e123b97561da04cb3ab27aa
Siemvc <144440423+Siemvc@users.noreply.github.com>
2025-04-22 19:58:34 +0200

    reduce memory usage

--- Deployment/MLModels.py
+++ Deployment/MLModels.py
@@ -44,7 +44,7 @@ def TrainModel(dataFrame, TrainOnColumns, typeOfModel, testSize = 0.2):
             'clf__reg_lambda': Real(0.0, 10.0),
             'clf__gamma': Real(0.0, 10.0)
             }
-            model = BayesSearchCV(pipe, searchSpace, cv=5, n_iter=20, scoring='roc_auc_ovr', random_state=1, refit=True)
+            model = BayesSearchCV(pipe, searchSpace, cv=2, n_iter=5, scoring='roc_auc_ovr', random_state=1, refit=True)
             model.fit(xTrain, yTrain)
         case 'Random Forest':
             RFModel = RandomForestClassifier(random_state=1)
@@ -55,13 +55,13 @@ def TrainModel(dataFrame, TrainOnColumns, typeOfModel, testSize = 0.2):
                 'min_samples_split': Integer(2, 10),
                 'min_samples_leaf': Integer(2, 10),
             }
-            model = BayesSearchCV(estimator=RFModel, search_spaces=searchSpace, n_iter=20, cv=5, random_state=1)
+            model = BayesSearchCV(estimator=RFModel, search_spaces=searchSpace, n_iter=5, cv=2, random_state=1)
             model.fit(xTrain, yTrain)
         case 'Logistic Regression':
-            model = LogisticRegression(max_iter=2000)
+            model = LogisticRegression(max_iter=100)
             model.fit(xTrain, yTrain)   
         case 'Neural Network':
             model = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                    hidden_layer_sizes=(5, 3), random_state=1, max_iter = 2000)
+                    hidden_layer_sizes=(5, 3), random_state=1, max_iter = 100)
             model.fit(xTrain, yTrain)
     return model
